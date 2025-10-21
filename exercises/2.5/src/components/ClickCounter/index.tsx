71e94c6fe80fe99161d84f925fa2538bdf2590ae
aya-janaa-vinci <aya.janaa@student.vinci.be>
2025-10-22 00:17:24 +0200

    new:ex2.5-events

--- exercises/2.5/src/components/ClickCounter/index.tsx
+++ exercises/2.5/src/components/ClickCounter/index.tsx
@@ -1,19 +1,26 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import "./ClickCounter.css";
 
 interface ClickCounterProps{
 title : string ;
 on10ClickMessage? : string;
+onMouseOverMessage? : string;
 }
 
 const ClickCounter = ({title,
-    on10ClickMessage = "You are a master in the art of clicking !"}
+    on10ClickMessage = "You are a master in the art of clicking !",
+onMouseOverMessage = "Please click on me now !"}
     :ClickCounterProps) => {
-    const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
+    const [isHovered, setIsHovered] = useState(false);
 
 return(
     <div className="card">
         <h4>{title}</h4>
-        <button onClick={() => setCount((count) => count + 1)}>
+        {isHovered ? <p>{onMouseOverMessage}</p> : null}
+        <button onClick={() => setCount((count) => count + 1)}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}>
           count is {count}
         </button>
         {count >= 10 ? <p>{on10ClickMessage}</p> : null}
