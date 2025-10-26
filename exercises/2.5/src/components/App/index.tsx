ee536b8578a565176f16a120a8f85e0982b70705
aya-janaa-vinci <aya.janaa@student.vinci.be>
2025-10-26 16:23:24 +0100

    new:ex2.5-eventsCorrection

--- exercises/2.5/src/components/App/index.tsx
+++ exercises/2.5/src/components/App/index.tsx
@@ -1,7 +1,8 @@
 
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import reactLogo from '../../assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import ClickCounter from '../ClickCounter';
 
 function App() {
 
@@ -16,6 +17,8 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+
+      <ClickCounter title='The best counter in the world !'/>
      
 
       <p className="read-the-docs">
