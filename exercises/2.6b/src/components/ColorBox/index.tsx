1a351b3d797ae0fdaad78fa297da0f1cccf95d2a
aya-janaa-vinci <aya.janaa@student.vinci.be>
2025-11-02 11:34:20 +0100

    ex 2.6b updated

--- exercises/2.6b/src/components/ColorBox/index.tsx
+++ exercises/2.6b/src/components/ColorBox/index.tsx
@@ -2,25 +2,25 @@ import { useState } from "react";
 
 const colors = ["red", "green", "blue", "yellow", "purple"];
 
-
 const ColorBox = () => {
-    const [currentColorIndex, setCurrentColorIndex] = useState(0);
+  const [currentColorIndex, setCurrentColorIndex] = useState(0);
 
-return (
+  return (
     <div
       className="color-box"
       style={{ backgroundColor: colors[currentColorIndex] }}
     >
-        <button className="color-box__button"
+      <button
+        className="color-box__button"
         onClick={() => {
-            setCurrentColorIndex((currentColorIndex + 1) % colors.length);
+          setCurrentColorIndex((currentColorIndex + 1) % colors.length);
         }}
-        >
-            {colors[(currentColorIndex + 1) % colors.length]} //texte du bouton, affiche la prochaine couleur
-        </button>
-        <h3>{colors[currentColorIndex]}</h3> //Affiche la couleur actuelle
+      >
+        {colors[(currentColorIndex + 1) % colors.length]}
+      </button>
+      <h3>{colors[currentColorIndex]}</h3>
     </div>
-);
+  );
 };
 
 export default ColorBox;
