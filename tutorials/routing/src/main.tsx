89d35bd4a7e5f62ba8260d70cc0637683f819fa9
aya-janaa-vinci <aya.janaa@student.vinci.be>
2025-11-08 22:38:02 +0100

    new tutorial : routing

---
+++ tutorials/routing/src/main.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { AboutPage, ContactPage, HomePage } from "./components/App";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import App from "./components/App";
+import { UserPage } from "./components/App";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "",
+        element: <HomePage />,
+      },
+      {
+        path: "about",
+        element: <AboutPage />,
+      },
+      {
+        path: "contact",
+        element: <ContactPage />,
+      },
+      {
+        path: "users/:userId",
+        element: <UserPage />,
+      }
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
