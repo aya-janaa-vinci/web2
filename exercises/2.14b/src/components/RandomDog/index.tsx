25ae7bb8f44e4fda0463c310d1eb1a56bfca5911
aya-janaa-vinci <aya.janaa@student.vinci.be>
2025-12-01 22:17:21 +0100

    new:ex2.14b-refactor

---
+++ exercises/2.14b/src/components/RandomDog/index.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import type { Dog } from "../../types";
+
+const RandomDog = () => {
+  const [dog, setDog] = useState<Dog | undefined>(undefined);
+
+  useEffect(() => {
+    const fetchDogImage = async () => {
+      try {
+        const response = await fetch("https://dog.ceo/api/breeds/image/random");
+
+        const doggy = await response.json();
+        setDog({
+          message: doggy.message ?? "No dog found",
+          status: doggy.status ?? "Error",
+        });
+      } catch (error) {
+        console.error("Failed to fetch dog image", error);
+        setDog({ message: "Failed to fetch dog image", status: "Error" });
+      }
+    };
+    fetchDogImage();
+  }, []);
+
+  if (!dog) {
+    return <p>Loading...</p>;
+  }
+  return (
+    <div>
+      <h3>Random dog</h3>
+      <img src={dog.message} alt="Random dog" style={{ maxHeight: 300 }} />
+    </div>
+  );
+};
+
+export default RandomDog;
